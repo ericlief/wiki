dcc67074a3a6ecd9ab91e04419cf75d5a138dd05
Eric Lief <ericlief@bossa>
2018-09-19 15:58:21 +0200

    new script

--- preproc_xml.py
+++ preproc_xml.py
@@ -1,10 +1,24 @@
-import bz2, sys
+import bz2, sys, io
+import xml.etree.ElementTree as et
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.corpora import WikiCorpus, MmCorpus
+
 
 file_in = sys.argv[1]
 file_out = sys.argv[2]
 
-with bz2.open(file_in, 'r') as f:
-    data = f.read() 
-    print(data)
-    
-    
+#with bz2.open(file_in, 'r') as f:
+    #data = f.read() 
+    #fh = io.StringIO(data.decode('utf8'))
+    #print(fh)
+    #tree = et.ElementTree()
+    #root = tree.parse(fh)
+    #pages = []
+    #for e in tree.getiterator('page'):
+        #pages.append(e)
+        
+        
+path_to_wiki_dump = datapath(file_in)
+corpus_path = get_tmpfile("wiki-corpus.mm")
+wiki = WikiCorpus(path_to_wiki_dump) # create word->word_id mapping, ~8h on full wiki
+MmCorpus.serialize(corpus_path, wiki) # another 8h, creates a file in MatrixMarket format and mapping    
